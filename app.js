51479e736afd40e10af6c6fa3acc6506f7058cf2
Grozdov Dmitry <GrozdDV@yandex.ru>
2023-07-08 00:36:45 +0300

    2.2. Middleware. Паттерн цепочка обязанностей

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const router = require('./routes')
 const { port } = require('./config.json')
 //проверка для html формы
-//app.use(express.static('./', {index: 'index.html'})).
+//app.use(express.static('./', {index: 'index.html'}))
 app.use(express.json())
 app.use(router)
 app.listen(port, ()=>{
